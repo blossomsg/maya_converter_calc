b56399cfd85645b01ce6c933de5253716a75ea11
blossomsg <38830385+blossomsg@users.noreply.github.com>
2019-12-15 20:47:20 +0530

    Message02: this for maya shelf

---
+++ maya_converter_calc_shelf.py
@@ -0,0 +1,26 @@
+import sys
+path = "D:\\All_Projs\\amazon_proj\\maya_converter_calc"#provide path where you are saving the script
+if path in sys.path:
+    print "path already exists"
+else:
+    print "path does not exist"
+    sys.path.append(path)
+
+def main():
+    """
+    The main function is to run the code and exit on recalling the tool
+    Returns:
+        None
+    """
+    # CAVEAT: "win" var is global so that it can be used in multiple situations in the script
+    global win
+    # CAVEAT: first it will try to close if the win exist
+    try:
+        win.close()
+    except:
+        pass
+    window = maya_converter_calc_func.MayaConverterCalcFunc()
+    # CAVEAT: if the exception is raised the window will be shown
+    window.show()
+
+main()
